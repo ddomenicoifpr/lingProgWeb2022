6a848e1493a7be0ed1abc4adf44981dba0cb23df
Daniel Di Domenico <ddomenico@inf.ufpel.edu.br>
2022-09-16 14:54:42 -0300

    Material aula 1

---
+++ php/cad_alunos_aula/index.php
@@ -0,0 +1,23 @@
+<?php
+#Nome do arquivo: index.php
+#Objetivo: monstrar os alunos cadastrados no sistema
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>SISTEMA DE CADASTRO DE ALUNOS</title>
+	<meta charset="UTF-8">
+</head>
+
+<body>
+   <center><h1 style="color: green;">SISTEMA DE CADASTRO DE ALUNOS</h1></center>
+   
+   <br><br><br><br><br>
+   
+   <a href="#"> Incluir Novo Aluno</a><br><br><br>
+   
+   <h4>RELAÇÃO DOS ALUNOS CADASTRADOS</h4>
+ 
+</body>
+</html>
